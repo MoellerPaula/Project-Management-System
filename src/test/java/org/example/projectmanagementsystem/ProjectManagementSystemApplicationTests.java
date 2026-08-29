aa4f1192c3736e4ca169f5f0275da6f051dc7d17
MoellerPaula <moeler.paula@gmail.com>
2026-08-29 13:00:32 +0200

    test: replace generated test with project test

--- src/test/java/org/example/projectmanagementsystem/ProjectManagementSystemApplicationTests.java
+++ src/test/java/org/example/projectmanagementsystem/ProjectManagementSystemApplicationTests.java
@@ -1,13 +1,13 @@
 package org.example.projectmanagementsystem;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class ProjectManagementSystemApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testExample() {
+		assertEquals(2, 1 + 1);
 	}
-
 }
