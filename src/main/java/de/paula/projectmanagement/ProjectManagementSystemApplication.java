7d3043029fc660ef3576924c257fd033790bd499
MoellerPaula <moeler.paula@gmail.com>
2026-09-05 10:00:09 +0200

    feat: add initial flyway migration

--- src/main/java/de/paula/projectmanagement/ProjectManagementSystemApplication.java
+++ src/main/java/de/paula/projectmanagement/ProjectManagementSystemApplication.java
@@ -1,4 +1,4 @@
-package org.example.projectmanagementsystem;
+package de.paula.projectmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/de/paula/projectmanagement/project/Project.java
@@ -0,0 +1,16 @@
+package de.paula.projectmanagement.project;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Project {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String name;
+}
